65d15b431a56603d8872c6568bf4a8eb6b362b43
vlads2 <quietDoppio@yandex.ru>
2025-02-08 19:18:26 +0300

    KTB-04. Определил data класс для хранения данных о строке. Определил разделитель в строках для последующего парсинга

--- src/main/kotlin/dictionary/file.kt
+++ src/main/kotlin/dictionary/file.kt
@@ -2,8 +2,22 @@ package dictionary
 
 import java.io.File
 
+data class Word(
+    val origWord: String,
+    val translatedWord: String,
+    val correctAnswerCount: Int = 0,
+)
+
 fun main() {
+    val dictionary = mutableListOf<Word>()
+
     val wordsFile = File("words")
     wordsFile.createNewFile()
-    wordsFile.forEachLine { println(it) }
+
+    wordsFile.forEachLine {
+        val line = it.split("|")
+        dictionary.add(Word(origWord = line[0], translatedWord = line[1]))
+    }
+
+    dictionary.forEach { println(it) }
 }
