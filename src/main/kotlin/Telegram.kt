0af1a3e48d978d0fdacf76c7ee151cfcffe58009
quietDoppio <quietDoppio@yandex.ru>
2025-02-23 23:04:55 +0300

    KTB-12 правки

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -2,8 +2,7 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-const val GET_ME = "getMe"
-const val GET_UPDATES = "getUpdates"
+
 fun main(args: Array<String>) {
     val botToken = args[0]
     val botUrl = "https://api.telegram.org/bot$botToken/%s"
@@ -11,16 +10,16 @@ fun main(args: Array<String>) {
     val client: HttpClient = HttpClient.newBuilder().build()
     val getMeRequest = HttpRequest
         .newBuilder()
-        .uri(URI.create(botUrl.format(GET_ME)))
+        .uri(URI.create(botUrl.format("getMe")))
         .build()
     val getUpdatesRequest: HttpRequest = HttpRequest
         .newBuilder()
-        .uri(URI.create(botUrl.format(GET_UPDATES)))
+        .uri(URI.create(botUrl.format("getUpdates")))
         .build()
 
     val getMeResponse: HttpResponse<String> = client.send(getMeRequest, HttpResponse.BodyHandlers.ofString())
     val getUpdatesResponse: HttpResponse<String> = client.send(getUpdatesRequest, HttpResponse.BodyHandlers.ofString())
 
-    println(getMeResponse.body())
-    println(getUpdatesResponse.body())
+    println(getMeResponse.body().toString())
+    println(getUpdatesResponse.body().toString())
 }
