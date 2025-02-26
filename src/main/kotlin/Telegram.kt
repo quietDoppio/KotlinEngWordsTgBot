c6b64e3d979c5fd8c9034da875d0d98f24c77746
vlads2 <quietDoppio@yandex.ru>
2025-02-26 15:24:15 +0300

    KTB-15. Отправил сообщение пользовотелю бота по команде

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -4,7 +4,6 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 fun main(args: Array<String>) {
-
     val botToken = args[0]
     var updateId = 0
     var message: String
@@ -19,10 +18,22 @@ fun main(args: Array<String>) {
             updateId = it.groupValues[1].toInt() + 1
             message = it.groupValues[2]
             println(message)
+            if (message == "/start") sendMessage(botToken, updates )
         }
     }
 }
+fun sendMessage(botToken: String, updates: String): String{
+    val updatesRegex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
+    val match = updatesRegex.find(updates)
+    val chatId = match?.groups?.get(1)?.value?.toInt()
 
+    val botUrl = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=Привет!+бот+ещё+не+работает!"
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(botUrl)).build()
+    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    return response.body()
+}
 fun getUpdates(botToken: String, updateId: Int): String{
     val botUrl = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
