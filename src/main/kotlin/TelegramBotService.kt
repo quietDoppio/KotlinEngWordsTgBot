4c6a8a37c080c00b40c88f6ac836338fc26e360c
quietDoppio <quietDoppio@yandex.ru>
2025-05-21 22:33:24 +0300

    KTB-1.5 Реализовал функционал отправления картинки в бот

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -4,10 +4,15 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.InputStream
+import java.math.BigInteger
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Random
 
 const val API_TELEGRAM_URL = "https://api.telegram.org/bot"
 const val CALLBACK_DATA_STATISTICS_CLICKED = "DATA_CALLBACK_STATISTICS_CLICKED"
@@ -79,7 +84,10 @@ class TelegramBotService(private val botToken: String) {
             replyMarkup = ReplyMarkup(
                 listOf(
                     listOf(
-                        InlineKeyboardButton(text = "Изучение слов", callbackData = CALLBACK_DATA_START_LEARNING_CLICKED),
+                        InlineKeyboardButton(
+                            text = "Изучение слов",
+                            callbackData = CALLBACK_DATA_START_LEARNING_CLICKED
+                        ),
                         InlineKeyboardButton(text = "Статистика", callbackData = CALLBACK_DATA_STATISTICS_CLICKED)
                     ),
                     listOf(
@@ -131,16 +139,6 @@ class TelegramBotService(private val botToken: String) {
         return response.body()
     }
 
-    private fun makeGetRequest(botUrl: String): HttpRequest =
-        HttpRequest.newBuilder().uri(URI.create(botUrl)).GET().build()
-
-    private fun makePostRequest(botUrl: String, jsonBodyString: String): HttpRequest {
-        return HttpRequest.newBuilder().uri(URI.create(botUrl))
-            .header("Content-type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString(jsonBodyString))
-            .build()
-    }
-
     fun downloadFile(filePath: String, fileName: String) {
         val urlFilePath = "${API_TELEGRAM_URL.dropLast(4)}/file/bot$botToken/$filePath"
         println(urlFilePath)
@@ -154,40 +152,112 @@ class TelegramBotService(private val botToken: String) {
 
     fun getFileRequest(fileId: String, json: Json): String {
         val urlGetFile = "$botUrl/getFile"
-        val requestBody = GetFileBody(fileId)
+        val requestBody = GetFileBody(fileId = fileId)
         val requestBodyString = json.encodeToString(requestBody)
         val request = makePostRequest(urlGetFile, requestBodyString)
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
         return response.body()
     }
+
+    fun sendPhotoByFileId(fileId: String, chatId: Long, hasSpoiler: Boolean = false ,json: Json): String{
+        val getFileBody = SendPhotoBody(chatId = chatId, fileId = fileId, hasSpoiler = hasSpoiler)
+        val jsonBody = json.encodeToString(getFileBody)
+
+        val request = makePostRequest("$botUrl/sendPhoto", jsonBody)
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        return response.body()
+    }
+    fun sendPhotoByFile(file: File, chatId: Long, hasSpoiler: Boolean = false): String {
+        val data: MutableMap<String, Any> = LinkedHashMap()
+        data["chat_id"] = chatId.toString()
+        data["photo"] = file.toPath()
+        data["has_spoiler"] = hasSpoiler
+        val boundary = BigInteger(35, Random()).toString()
+
+        val request: HttpRequest = HttpRequest.newBuilder()
+            .uri(URI.create("$botUrl/sendPhoto"))
+            .postMultipartFormData(boundary, data)
+            .build()
+
+        val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+        return response.body()
+    }
+
+    fun HttpRequest.Builder.postMultipartFormData(boundary: String, data: Map<String, Any>): HttpRequest.Builder {
+        val byteArrays = ArrayList<ByteArray>()
+        val separator = "--$boundary\r\nContent-Disposition: form-data; name=".toByteArray(StandardCharsets.UTF_8)
+
+        for (entry in data.entries){
+            byteArrays.add(separator)
+            when(entry.value){
+                is Path -> {
+                    val path = entry.value as Path
+                    val mimeType = Files.probeContentType(path)
+                    byteArrays.add(
+                        "\"${entry.key}\"; filename=\"${path.fileName}\"\r\nContent-Type: $mimeType\r\n\r\n".toByteArray(StandardCharsets.UTF_8)
+                    )
+                    byteArrays.add(Files.readAllBytes(path))
+                    byteArrays.add("\r\n".toByteArray(StandardCharsets.UTF_8))
+                }
+                else -> {
+                    byteArrays.add("\"${entry.key}\"\r\n\r\n${entry.value}\r\n".toByteArray(StandardCharsets.UTF_8))
+                }
+            }
+
+        }
+        byteArrays.add("--$boundary--".toByteArray(StandardCharsets.UTF_8))
+        this.header("Content-type", "multipart/form-data;boundary=$boundary")
+            .POST(HttpRequest.BodyPublishers.ofByteArrays(byteArrays))
+
+        return this
+    }
+
+    private fun makeGetRequest(botUrl: String): HttpRequest =
+        HttpRequest.newBuilder().uri(URI.create(botUrl)).GET().build()
+
+    private fun makePostRequest(botUrl: String, jsonBodyString: String): HttpRequest {
+        return HttpRequest.newBuilder().uri(URI.create(botUrl))
+            .header("Content-type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(jsonBodyString))
+            .build()
+    }
+
+    @Serializable
+    data class SendMessageBody(
+        @SerialName("chat_id")
+        val chatId: Long,
+        @SerialName("text")
+        val text: String,
+        @SerialName("reply_markup")
+        val replyMarkup: ReplyMarkup? = null,
+    )
+
+    @Serializable
+    data class GetFileBody(
+        @SerialName("file_id")
+        val fileId: String,
+    )
+    @Serializable
+    data class SendPhotoBody(
+        @SerialName("chat_id")
+        val chatId: Long?,
+        @SerialName("photo")
+        val fileId: String,
+        @SerialName("has_spoiler")
+        val hasSpoiler: Boolean
+    )
+
+    @Serializable
+    data class ReplyMarkup(
+        @SerialName("inline_keyboard")
+        val inlineKeyboard: List<List<InlineKeyboardButton>>
+    )
+
+    @Serializable
+    data class InlineKeyboardButton(
+        @SerialName("text")
+        val text: String,
+        @SerialName("callback_data")
+        val callbackData: String
+    )
 }
-
-@Serializable
-data class SendMessageBody(
-    @SerialName("chat_id")
-    val chatId: Long,
-    @SerialName("text")
-    val text: String,
-    @SerialName("reply_markup")
-    val replyMarkup: ReplyMarkup? = null,
-)
-
-@Serializable
-data class GetFileBody(
-    @SerialName("file_id")
-    val fileId: String
-)
-
-@Serializable
-data class ReplyMarkup(
-    @SerialName("inline_keyboard")
-    val inlineKeyboard: List<List<InlineKeyboardButton>>
-)
-
-@Serializable
-data class InlineKeyboardButton(
-    @SerialName("text")
-    val text: String,
-    @SerialName("callback_data")
-    val callbackData: String
-)
